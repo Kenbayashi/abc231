50c0d9efc62846f9dc65e32cc5a7dbd0672aa286
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-05 20:05:53 +0900

    c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,3 +1,19 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        q: usize,
+        a: [u64; n],
+        x: [u64; q],
+    }
+
+    let get_ans = |num_x| {
+        let ans = a.iter()
+                   .filter(|&num_a| num_x <= num_a)
+                   .count();
+
+        println!("{}", ans);
+    };
+
+    x.iter()
+     .for_each(get_ans);
 }
