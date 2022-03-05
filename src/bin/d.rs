f08e53aaa5f006b4d995c8d5f384349951a3bb21
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-05 20:16:43 +0900

    d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        _: usize,
+        m: usize,
+        xs: [u64; m * 2],
+    }
+
+    let mut map = HashMap::<u64, u8>::new();
+
+    xs.into_iter()
+      .for_each(|x| *map.entry(x).or_default() += 1);
+
+    let ans = map.into_iter()
+                 .fold(true, |acc, (_, count)| acc && count <= 2);
+
+    println!("{}", if ans {"Yes"} else {"No"});
 }
