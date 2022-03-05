8134eb4025123eb569ccd3134fbd62d5fb9bbb4d
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-05 21:19:09 +0900

    e

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,3 +1,25 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        x: u64,
+        coins: [u64; n],
+    }
+
+    let ans = count_coin(0, n, &coins, x);
+
+    println!("{}", ans);
+}
+
+fn count_coin(index: usize, n: usize, coins: &Vec<u64>, x: u64) -> u64 {
+    if index == n - 1 {
+        return x / coins[index];
+    }
+
+    let payment = x % coins[index + 1];
+    let charge  = coins[index + 1] - payment;
+
+    let under_case = count_coin(index + 1, n, coins, x - payment) + payment / coins[index];
+    let over_case = count_coin(index + 1, n, coins, x + charge) + charge / coins[index];
+
+    u64::min(under_case, over_case)
 }
